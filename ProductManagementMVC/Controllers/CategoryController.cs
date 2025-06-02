552238e74f0722b53c17eba7ff7e54e6c95efe50
tamBug <nhutam181103@gmail.com>
2025-06-02 14:52:13 +0700

    feat: Category-View

---
+++ ProductManagementMVC/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductManagementMVC.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly HttpClient _httpClient;
+		private const string _baseUrl = "https://localhost:7228/api";
+
+		public CategoryController()
+		{
+            _httpClient = new HttpClient();
+        }
+		public async Task<IActionResult> Index()
+		{
+			var categories = await _httpClient.GetFromJsonAsync<List<Category>>($"{_baseUrl}/categories");
+			return View(categories);
+		}
+	}
+}
